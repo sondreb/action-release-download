ff0e2a4a19143b36459d7f4b3b65b870d7035baf
SondreB <sondre@outlook.com>
2021-03-08 02:58:27 +0100

    Add user agent to requests

--- action.js
+++ action.js
@@ -77,8 +77,13 @@ const http = require('https');
         //     }
         // }
 
+        const options = {
+            url: url,
+            headers: { 'User-Agent': 'sondreb/action-release-download' }
+          };
+
         // Fetch the assets JSON file to find all artifacts to download
-        http.get(url, res => {
+        http.get(options, res => {
             let data = [];
             const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
             console.log('Status Code:', res.statusCode);
